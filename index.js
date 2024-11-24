a8d413786d7a81fc7af3bd7eb119a43b39b49453
PETERS OLUWASEYI GABRIEL <gpittaz67@gmail.com>
2024-11-24 17:26:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const sessionModel=require("./model/sessiosDB")
 const ticktModel=require("./model/ticketDb")
 
 //CONFIGS
-app.use(cors({
+app.use('*',cors({
     origin:"http://localhost:5173",
     methods:["GET", "POST", "PUT", "DELETE"],
     credentials:true,
