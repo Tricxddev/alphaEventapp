bcd7c31b51ca1bbef4edcbc83e88b33114318fdf
unknown <gpittaz67@gmail.com>
2025-07-11 12:33:59 +0100

    event cretn update

--- routes/creatEventRout.js
+++ routes/creatEventRout.js
@@ -1,8 +1,11 @@
 const creatEventFXN=require("../controllers/creatEnvt")
 const express = require("express")
 const { Module } = require("module")
+const multer = require("multer");
+const upload = multer().none(); // no files, just fields
+
 const router = express.Router()
 
-router.post("/createVnt/:userID",creatEventFXN)
+router.post("/createVnt/:userID",upload,creatEventFXN)
 
 module.exports=router
