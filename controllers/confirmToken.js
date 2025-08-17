08dbac756fba67f319e5a7974bd55690dc9ea0f8
unknown <gpittaz67@gmail.com>
2025-08-17 13:47:40 +0100

    cors true all * updtl

--- controllers/confirmToken.js
+++ controllers/confirmToken.js
@@ -11,7 +11,7 @@ const confirmTokenFXN=async(req,res)=>{
     const {email}=req.params;
     const verifyID= await allUserModel.findOne({email});
     const getOTP=await Otp.findOne({email})
-
+    console.log("request:",req.body)
     console.log("userToken:", userToken);
     if(!verifyID){
       res.status(403).json({msg:"INVALID ACTION"})
