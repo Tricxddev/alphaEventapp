3e129d1c7b85611ffb6eabcf0579f7ed3b35c8e1
unknown <gpittaz67@gmail.com>
2025-09-14 08:35:33 +0100

    cors true all * updt for local host FND FRNTEND request2

--- index.js
+++ index.js
@@ -56,11 +56,16 @@ dotenv.config()
 //   methods:["GET", "POST", "PUT", "DELETE","PATCH"],
 //   credentials: true,
 // };
-app.use('*',corsOptions({
-    origin:"http://localhost:5174",
-    methods:["GET", "POST", "PUT", "DELETE","PATCH"],
-    credentials:true,
- }))
+const corsOptions = {
+  origin: "http://localhost:5174", 
+  methods:["GET", "POST", "PUT", "DELETE","PATCH"],
+  credentials: true,
+};
+// app.use('*',corsOptions({
+//     origin:"http://localhost:5174",
+//     methods:["GET", "POST", "PUT", "DELETE","PATCH"],
+//     credentials:true,
+//  }))
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
